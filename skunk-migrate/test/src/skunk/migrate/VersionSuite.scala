7a7b2e6eb9959b1727eb0f499ff27bffaf971ed1
Kieren Davies <kieren@kdavi.es>
2022-08-07 11:57:25 +0000

    Move test migration to its own source file

--- skunk-migrate/test/src/skunk/migrate/VersionSuite.scala
+++ skunk-migrate/test/src/skunk/migrate/VersionSuite.scala
@@ -1,18 +1,11 @@
 package skunk.migrate
 
-import cats.Eval
 import munit.FunSuite
-import skunk.Session
-import skunk.Transaction
+
+import testmigrations.`2022-07-28T11:51:57Z__TestMigration`
 
 class VersionSuite extends FunSuite:
   test("derive from class name") {
-    object `2022-07-28T11:51:57Z__TestMigration` extends ReversibleMigration[Eval] derives Version.FromClass:
-      def up(s: Session[Eval], xa: Transaction[Eval]): Eval[Unit] = Eval.Unit
-      def down(s: Session[Eval], xa: Transaction[Eval]): Eval[Unit] = Eval.Unit
-
-    val m = `2022-07-28T11:51:57Z__TestMigration`
-
-    assert(clue(m.timestamp) == 1659009117)
-    assert(clue(m.name) == "TestMigration")
+    assert(clue(`2022-07-28T11:51:57Z__TestMigration`.timestamp) == 1659009117)
+    assert(clue(`2022-07-28T11:51:57Z__TestMigration`.name) == "TestMigration")
   }
---
+++ skunk-migrate/test/src/skunk/migrate/testmigrations/2022-07-28T11:51:57Z__TestMigration.scala
@@ -0,0 +1,11 @@
+package skunk.migrate.testmigrations
+
+import cats.Eval
+import skunk.Session
+import skunk.Transaction
+import skunk.migrate.ReversibleMigration
+import skunk.migrate.Version
+
+object `2022-07-28T11:51:57Z__TestMigration` extends ReversibleMigration[Eval] derives Version.FromClass:
+  def up(s: Session[Eval], xa: Transaction[Eval]): Eval[Unit] = Eval.Unit
+  def down(s: Session[Eval], xa: Transaction[Eval]): Eval[Unit] = Eval.Unit
